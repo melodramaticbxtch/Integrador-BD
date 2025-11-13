76b6aa1f0f015387a7bd4dced10b11b00effe10c
melodramaticbxtch <kiarabgamarra@gmail.com>
2025-11-13 19:47:32 -0300

    Tabla Sucursal

--- Integrador_BD.sql
+++ Integrador_BD.sql
@@ -1,1 +1,14 @@
-CREATE DATABASE financiamiento_ra;
+USE financiamiento_ra;
+CREATE TABLE ´Sucursal´ (
+	id_sucursal int UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+	nombre_sucursal varchar(255),
+	localidad varchar(255),
+	provincia varchar(255),
+	telefono_sucursal varchar(255), 
+	direccion varchar(255), 
+	fecha_de_alta timestamp, 
+	fecha_de_baja timestamp,
+	fecha_de_modificacion timestamp,
+	creado_por varchar(255), 
+	modificado_por varchar (255)
+)
