b724e5b4f7cc43c0bc36cce64c2f70b2e0218fd2
Matias Viana Carlsson <matiascarlsson1@gmail.com>
2025-11-15 08:32:01 -0300

    [UPD] Reformat code

--- Integrador_BD.sql
+++ Integrador_BD.sql
@@ -1,157 +1,162 @@
 USE financiamiento_ra;
-CREATE TABLE `Sucursal` (
-    `id_sucursal` INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT, 
-    `nombre_sucursal` VARCHAR(255),
-    `localidad` VARCHAR(255),
-    `provincia` VARCHAR(255),
-    `telefono_sucursal` VARCHAR(255), 
-    `direccion` VARCHAR(255), 
-    `fecha_de_alta` TIMESTAMP, 
-    `fecha_de_baja` TIMESTAMP,
-    `fecha_de_modificacion` TIMESTAMP,
-    `creado_por` VARCHAR(255), 
-    `modificado_por` VARCHAR(255)
+CREATE TABLE sucursal
+(
+    id_sucursal           INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nombre_sucursal       VARCHAR(255),
+    localidad             VARCHAR(255),
+    provincia             VARCHAR(255),
+    telefono_sucursal     VARCHAR(255),
+    direccion             VARCHAR(255),
+    fecha_de_alta         TIMESTAMP,
+    fecha_de_baja         TIMESTAMP,
+    fecha_de_modificacion TIMESTAMP,
+    creado_por            VARCHAR(255),
+    modificado_por        VARCHAR(255)
 );
 
-CREATE TABLE `Garante` (
-    `id_garante` INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `nombre` VARCHAR(255),
-    `apellido` VARCHAR(255),
-    `DNI_garante` VARCHAR(255),
-    `fecha_de_alta` TIMESTAMP,
-    `fecha_de_baja` TIMESTAMP,
-    `fecha_de_modificacion` TIMESTAMP,
-    `creado_por` VARCHAR(255),
-    `modificado_por` VARCHAR(255),
-    `id_solicitud` INT,
-    FOREIGN KEY (`id_solicitud`) REFERENCES `Solicitud`(`id_solicitud`)
+CREATE TABLE garante
+(
+    id_garante            INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nombre                VARCHAR(255),
+    apellido              VARCHAR(255),
+    dni_garante           VARCHAR(255),
+    fecha_de_alta         TIMESTAMP,
+    fecha_de_baja         TIMESTAMP,
+    fecha_de_modificacion TIMESTAMP,
+    creado_por            VARCHAR(255),
+    modificado_por        VARCHAR(255),
+    id_solicitud          INT,
+    FOREIGN KEY (id_solicitud) REFERENCES solicitud (id_solicitud)
 );
 
-CREATE TABLE `Solicitud` (
-    `id_solicitud` INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `monto` DECIMAL,
-    `destino` VARCHAR(255), 
-    `fecha_solicitud` TIMESTAMP,
-    `estado` BOOLEAN,
-    `motivo_estado` VARCHAR(255),
-    `fecha_de_alta` TIMESTAMP, 
-    `fecha_de_baja` TIMESTAMP,
-    `fecha_de_modificacion` TIMESTAMP,
-    `creado_por` VARCHAR(255),
-    `modificado_por` VARCHAR(255)
+CREATE TABLE solicitud
+(
+    id_solicitud          INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    monto                 DECIMAL,
+    destino               VARCHAR(255),
+    fecha_solicitud       TIMESTAMP,
+    estado                BOOLEAN,
+    motivo_estado         VARCHAR(255),
+    fecha_de_alta         TIMESTAMP,
+    fecha_de_baja         TIMESTAMP,
+    fecha_de_modificacion TIMESTAMP,
+    creado_por            VARCHAR(255),
+    modificado_por        VARCHAR(255)
 );
 
-CREATE TABLE `Cliente` (
-    `id_cliente` INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `nombre_cliente` VARCHAR(255),
-    `apellido_cliente` VARCHAR(255),
-    `documento` VARCHAR(255),  
-    `tipo_de_persona` ENUM('juridica', 'fisica'),
-    `ingreso_declarado` DECIMAL,
-    `domicilio` VARCHAR(255),
-    `email_cliente` VARCHAR(255),
-    `telefono_cliente` VARCHAR(255),
-    `fecha_de_alta` TIMESTAMP,
-    `fecha_de_baja` TIMESTAMP,
-    `fecha_de_modificacion` TIMESTAMP,
-    `creado_por` VARCHAR(255),
-    `modificado_por` VARCHAR(255)
+CREATE TABLE cliente
+(
+    id_cliente            INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nombre_cliente        VARCHAR(255),
+    apellido_cliente      VARCHAR(255),
+    documento             VARCHAR(255),
+    tipo_de_persona       ENUM ('juridica', 'fisica'),
+    ingreso_declarado     DECIMAL,
+    domicilio             VARCHAR(255),
+    email_cliente         VARCHAR(255),
+    telefono_cliente      VARCHAR(255),
+    fecha_de_alta         TIMESTAMP,
+    fecha_de_baja         TIMESTAMP,
+    fecha_de_modificacion TIMESTAMP,
+    creado_por            VARCHAR(255),
+    modificado_por        VARCHAR(255)
 );
 
-    
-CREATE TABLE `Producto_campana` (
-
-
-
-CREATE TABLE `Producto_financiero` (
-`id_producto_financiero` integer UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-`nombre_producto_financiero` varchar(255),
-`descripcion` varchar(255),
-`limite_max` decimal(10,2),
-`limite_min` decimal(10,2),
-`requisitos` varchar(255),
-`tasa_base` decimal,
-`fecha_de_alta` timestamp,
-`fecha_de_baja` timestamp,
-`fecha_de_modificacion` timestamp,
-`creado_por` varchar(255),
-`modificado_por` varchar(255)
+CREATE TABLE producto_financiero
+(
+    id_producto_financiero     INTEGER UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nombre_producto_financiero VARCHAR(255),
+    descripcion                VARCHAR(255),
+    limite_max                 DECIMAL(10, 2),
+    limite_min                 DECIMAL(10, 2),
+    requisitos                 VARCHAR(255),
+    tasa_base                  DECIMAL,
+    fecha_de_alta              TIMESTAMP,
+    fecha_de_baja              TIMESTAMP,
+    fecha_de_modificacion      TIMESTAMP,
+    creado_por                 VARCHAR(255),
+    modificado_por             VARCHAR(255)
 );
 
-CREATE TABLE Credito (
-`id_credito integer` integer UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-`id_cliente` int not null,
-`id_producto` int not null,
-`id_solicitud` int not null,
+CREATE TABLE credito
+(
+    id_credito            INTEGER UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id_cliente            INT                        NOT NULL,
+    id_producto           INT                        NOT NULL,
+    id_solicitud          INT                        NOT NULL,
 
--- Clave foránea autorreferenciada (crédito padre)
-`id_credito_padre` int not null,
-`monto_otorgado` decimal,
-`fecha_inicio` timestamp,
-`fecha_fin` timestamp,
-`tasa_aplicada` decimal,
-`plazo_devolucion` int,
-`estado_crediticio` varchar(255),
-`fecha_de_alta` timestamp,
-`fecha_de_baja` timestamp,
-`fecha_de_modificacion` timestamp,
-`creado_por` varchar(255),
-`modificado_por` varchar(255),
-FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente),
-FOREIGN KEY (id_producto) REFERENCES Producto(id_producto),
-FOREIGN KEY (id_solicitud) REFERENCES Solicitud(id_solicitud),
-FOREIGN KEY (id_credito_padre) REFERENCES credito(id_credito)
+    -- Clave foránea autorreferenciada (crédito padre)
+    id_credito_padre      INT                        NOT NULL,
+    monto_otorgado        DECIMAL,
+    fecha_inicio          TIMESTAMP,
+    fecha_fin             TIMESTAMP,
+    tasa_aplicada         DECIMAL,
+    plazo_devolucion      INT,
+    estado_crediticio     VARCHAR(255),
+    fecha_de_alta         TIMESTAMP,
+    fecha_de_baja         TIMESTAMP,
+    fecha_de_modificacion TIMESTAMP,
+    creado_por            VARCHAR(255),
+    modificado_por        VARCHAR(255),
+    FOREIGN KEY (id_cliente) REFERENCES cliente (id_cliente),
+    FOREIGN KEY (id_producto) REFERENCES producto (id_producto),
+    FOREIGN KEY (id_solicitud) REFERENCES solicitud (id_solicitud),
+    FOREIGN KEY (id_credito_padre) REFERENCES credito (id_credito)
 );
 
-CREATE TABLE `Historial_de_tasas` (
-`id_historial` integer UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-`tasa` decimal(10,2),
-`fecha_inicio` timestamp,
-`fecha_fin` timestamp,
-`fecha_de_alta` timestamp,
-`fecha_de_baja` timestamp,
-`fecha_de_modificacion` timestamp,
-`creado_por` varchar(255),
-`modificado_por` varchar(255)
+CREATE TABLE historial_de_tasas
+(
+    id_historial          INTEGER UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    tasa                  DECIMAL(10, 2),
+    fecha_inicio          TIMESTAMP,
+    fecha_fin             TIMESTAMP,
+    fecha_de_alta         TIMESTAMP,
+    fecha_de_baja         TIMESTAMP,
+    fecha_de_modificacion TIMESTAMP,
+    creado_por            VARCHAR(255),
+    modificado_por        VARCHAR(255)
 );
 
-CREATE TABLE `Cuota` (
-`id_cuota` integer UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-`id_credito` int not null,
-`numero_cuota` int,
-`fecha_de_emision` timestamp,
-`fecha_de_vencimiento` timestamp,
-`monto_total` decimal(10,2),
-`estado` boolean,
-`fecha_de_alta` timestamp,
-`fecha_de_baja` timestamp,
-`fecha_de_modificacion` timestamp,
-`creado_por` varchar(255),
-`modificado_por` varchar(255),
-FOREIGN KEY (id_credito) REFERENCES Credito(id_credito)
+CREATE TABLE cuota
+(
+    id_cuota              INTEGER UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id_credito            INT                        NOT NULL,
+    numero_cuota          INT,
+    fecha_de_emision      TIMESTAMP,
+    fecha_de_vencimiento  TIMESTAMP,
+    monto_total           DECIMAL(10, 2),
+    estado                BOOLEAN,
+    fecha_de_alta         TIMESTAMP,
+    fecha_de_baja         TIMESTAMP,
+    fecha_de_modificacion TIMESTAMP,
+    creado_por            VARCHAR(255),
+    modificado_por        VARCHAR(255),
+    FOREIGN KEY (id_credito) REFERENCES credito (id_credito)
 );
 
-CREATE TABLE `Pago` (
-`id_pago` integer UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-`id_cuota` int not null,
-`fecha_de_pago` timestamp,
-`monto_pagado` decimal,
-`demora` int,
-`penalizacion_mora` decimal(10,2),
-`fecha_de_alta` timestamp,
-`fecha_de_baja` timestamp,
-`fecha_de_modificacion` timestamp,
-`creado_por` varchar(255),
-`modificado_por` varchar(255),
-FOREIGN KEY (id_cuota) REFERENCES Cuota(id_cuota)
+CREATE TABLE pago
+(
+    id_pago               INTEGER UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id_cuota              INT                        NOT NULL,
+    fecha_de_pago         TIMESTAMP,
+    monto_pagado          DECIMAL,
+    demora                INT,
+    penalizacion_mora     DECIMAL(10, 2),
+    fecha_de_alta         TIMESTAMP,
+    fecha_de_baja         TIMESTAMP,
+    fecha_de_modificacion TIMESTAMP,
+    creado_por            VARCHAR(255),
+    modificado_por        VARCHAR(255),
+    FOREIGN KEY (id_cuota) REFERENCES cuota (id_cuota)
 );
 
-CREATE TABLE `Metodo_de_pago` (
-`id_metodo` integer UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-`metodo` enum("Efectivo", "Trajeta", "Transferencia") not null,
-`fecha_de_alta` timestamp,
-`fecha_de_baja` timestamp,
-`fecha_de_modificacion` timestamp,
-`creado_por` varchar(255),
-`modificado_por` varchar(255)
+CREATE TABLE metodo_de_pago
+(
+    id_metodo             INTEGER UNIQUE PRIMARY KEY                    NOT NULL AUTO_INCREMENT,
+    metodo                ENUM ('Efectivo', 'Trajeta', 'Transferencia') NOT NULL,
+    fecha_de_alta         TIMESTAMP,
+    fecha_de_baja         TIMESTAMP,
+    fecha_de_modificacion TIMESTAMP,
+    creado_por            VARCHAR(255),
+    modificado_por        VARCHAR(255)
 );
